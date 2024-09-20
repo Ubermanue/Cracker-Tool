dd27f1f5a4f9d8a7521312188b68bdab7e8e36a7
Ubermanue <rinz135orugejehe@gmail.com>
2024-09-21 05:49:28 +0800

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -10,8 +10,8 @@ pkg install p7zip
 pip install pyfiglet
 pip install bs4
 pip install lolcat
-#pip install -i https://test.pypi.org/simple/ cracker-say
-#pip install git+https://github.com/cracker911181/cracker_say
+#pip install -i https://test.pypi.org/simple/ ubermanue-say
+#pip install git+https://github.com/ubermanue/ubermanue_say
 pip install cracker-saying
 pip install cowsay 
 pip install youtube_dl
